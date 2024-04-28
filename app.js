b72e9e1450301b71caabc55cfe965e2e41f5a7b4
uda224 <uda224@lehigh.edu>
2024-04-28 17:56:23 -0400

    board set up

---
+++ app.js
@@ -0,0 +1,14 @@
+// Udita Agarwal
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(
+  path.resolve(__dirname, "public")
+));
+
+app.listen(3000, () => console.log("Starting Wordle"));
+
+
