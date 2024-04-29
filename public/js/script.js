fd26d5e225bf434d15328fb6a8666cd5f4497387
uda224 <uda224@lehigh.edu>
2024-04-29 16:02:04 -0400

    clickable keys

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 $(()=> {
     createSquares();
-
+    clickableKeys();
 })
 
 
@@ -12,5 +12,33 @@ function createSquares(){
         square.setAttribute("id", i+1);
         gameBoard.appendChild(square);
     }
+}
 
+function clickableKeys(){
+    const keysFirstRow = document.getElementById("firstrow").querySelectorAll("button");
+
+    for(let i = 0; i < keysFirstRow.length; i++){
+        console.log("hii");
+        keysFirstRow[i].addEventListener("click", function() {
+            console.log(keysFirstRow[i].id);
+        });
+    }
+
+    const keysSecondRow = document.getElementById("secondrow").querySelectorAll("button");
+
+    for(let i = 0; i < keysSecondRow.length; i++){
+        console.log("hii");
+        keysSecondRow[i].addEventListener("click", function() {
+            console.log(keysSecondRow[i].id);
+        });
+    }
+
+    const keysThirdRow = document.getElementById("thirdrow").querySelectorAll("button");
+
+    for(let i = 0; i < keysThirdRow.length; i++){
+        console.log("hii");
+        keysThirdRow[i].addEventListener("click", function() {
+            console.log(keysThirdRow[i].id);
+        });
+    }
 }
