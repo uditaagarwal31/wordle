1535006c16cd7192ef190fce2ccd590d7e2c1abf
uda224 <uda224@lehigh.edu>
2024-04-30 17:48:16 -0400

    working mistly!!

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,16 @@
 
 
 $(()=> {
+    const wordslist = [
+        "abode", "bloom", "chirp", "dwarf", "ethos", "flint", "gawky", "havoc", "ideal", "jolly", "kneel", "lumen", "mirth", "nymph", "opine", "prism", "quark", "rover", "swoon", "trove", "ulcer", "vivid", "waltz", "xenon", "yacht", "zebra", "amity", "bluff", "crisp", "droll", "exult", "fjord", "grasp", "humor", "inlay", "junto", "knoll", "lemma", "mossy", "novel", "ombre", "pique", "quire", "rebar", "spunk", "thyme", "usher", "valor", "wryly", "yodel", "zesty", "agape", "brisk", "cleft", "droit", "equip", "flume", "gloat", "hoard", "islet", "jazzy", "koala", "lofty", "modem", "nadir", "olden", "plumb", "quirk", "rerun", "shard", "tryst", "unfed", "viper", "whack", "xylem", "yucky", "zippy", "argon", "budge", "clank", "dusky", "elope", "fleck", "glyph", "hitch", "input", "joint", "knack", "lemur", "mango", "notch", "oxide", "plush", "query", "rival", "scamp", "tunic", "uncut", "vouch", "woven", "yeast", "zonal", "aloft", "banjo", "cynic", "duvet", "ennui", "flair", "gorge", "hovel", "irate", "junto",
+        "align", "baste", "chase", "decoy", "eerie", "fable", "glide", "harsh", "inbox", "juror", "knack", "latch", "moose", "nerve", "onset", "paste", "quilt", "ranch", "siren", "tease", "undue", "vista", "witty", "yield", "zonal", "adept", "badge", "crane", "dicey", "elude", "feign", "graft", "hinge", "idyll", "joker", "karma", "lucid", "mimic", "noble", "orbit", "pause", "quiet", "renew", "salty", "towel", "upper", "vivid", "wince", "xeric", "youth", "zealot", "acrid", "bribe", "cower", "dandy", "easel", "facet", "giddy", "heist", "infer", "junto", "knoll", "livid", "maxim", "niche", "occur", "proxy", "quest", "roast", "smelt", "tumor", "udder", "vegan", "worry", "xenon", "young", "zilch", "amble", "bison", "cabin", "diode", "eject", "flint", "groom", "hyena", "issue", "jumpy", "koala", "leapt", "merge", "naive", "opium", "pride", "quash", "relic", "stint", "tulip", "unity", "vault", "whirl", "xenon", "yarns", "zesty", "angst", "blitz", "champ", "dowel", "event", "flora", "gauze", "hoist", "impel", "jewel"];
     const guessedWords = [[]];
     let letterGuessesCount = 1;
-    let word = "dairy";
+    let randomIndex = Math.floor(Math.random() * 200);
+    let word = wordslist[randomIndex];
+    
+    console.log(randomIndex);
+    console.log(word);
     let wordGuessesCount = 1;
 
     createSquares();
@@ -124,17 +131,46 @@ $(()=> {
 
     function checkLetters(currentWord){
         let startIndex = letterGuessesCount - 5;
+        let time = 150;
+        let upperCaseCurrentWord = currentWord.toUpperCase();
         for(let i = 0; i < 5; i++){
-            let currentSq = document.getElementById(String(startIndex));
-            currentSq.classList.add("animate_flipInX");
-            
+            let currentButtonLetter = document.getElementById(upperCaseCurrentWord[i]);
+            setTimeout(()=>{
+                let currentSq = document.getElementById(String(startIndex));
+                
+                currentSq.classList.add("animate__flipInX");
+                
+                if(currentWord[i] === word[i]){
+                    console.log("match", currentWord[i]);
+                    currentSq.style = `background-color: green`;
+                  //  currentButtonLetter.style.backgroundColor = "green";
+                } else if (currentWord[i] !== word[i] && word.includes(currentWord[i])){
+                    currentSq.style = `background-color: yellow`;
+                  //  currentButtonLetter.style.backgroundColor = "grey";
+                } else {
+                    currentSq.style = `background-color: grey`;
+                }
+                startIndex++;
+            }, time*i);
             if(currentWord[i] === word[i]){
-                console.log("match", currentWord[i]);
-                currentSq.style = `background-color: green`;
+                currentButtonLetter.style.backgroundColor = "green";
+            } else if (currentWord[i] !== word[i] && word.includes(currentWord[i])){
+                currentButtonLetter.style.backgroundColor = "yellow";
             } else {
-                currentSq.style = `background-color: grey`;
+                currentButtonLetter.style.backgroundColor = "grey";
             }
-            startIndex++;
+
+
+            // let currentSq = document.getElementById(String(startIndex));
+            // currentSq.classList.add("animate__flipInX");
+            
+            // if(currentWord[i] === word[i]){
+            //     console.log("match", currentWord[i]);
+            //     currentSq.style = `background-color: green`;
+            // } else {
+            //     currentSq.style = `background-color: grey`;
+            // }
+            // startIndex++;
         }
     }
 
